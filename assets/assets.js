505b0c7daccd09ab83d7be53bb0b3c48ccc104ad
Aryankpoor <kapooraryan0419@gmail.com>
2024-10-02 00:49:33 +0530

    added old metamorpheus react elements

---
+++ assets/assets.js
@@ -0,0 +1,17 @@
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import gemini_icon from './gemini_icon.png'
+import sound_icon from './sound_icon.png'
+import send_icon from './send_icon.png'
+import message_icon from './message_icon.png'
+import github_icon from './github_icon.png'
+
+export const assets = {
+    menu_icon,
+    plus_icon,
+    gemini_icon,
+    sound_icon,
+    send_icon,
+    message_icon,
+    github_icon
+}
